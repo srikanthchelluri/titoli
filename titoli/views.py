83a8bec460f54f9d8ba0ee63f7a9852612cb743d
Srikanth Chelluri <schellluri.developer@gmail.com>
2018-03-24 12:55:07 -0400

    index

--- titoli/views.py
+++ titoli/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from api.models import Game
 
 # Create your views here.
 def index(request):
 	return render(request, 'index.html')
-	
